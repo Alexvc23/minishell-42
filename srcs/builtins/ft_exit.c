e9aef2aac730f4ddc6135588d8854b9a9d342561
Ross <floriandevigne1@hotmail.fr>
2022-08-18 20:41:57 +0200

    fixing exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: fdevigne <fdevigne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 15:25:13 by fdevigne          #+#    #+#             */
-/*   Updated: 2022/08/18 16:09:50 by fdevigne         ###   ########.fr       */
+/*   Updated: 2022/08/18 19:07:56 by fdevigne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,11 @@ static int	ft_strnum(char *str)
 	{
 		if (!ft_isdigit(str[i]))
 		{
+			ft_putstr_fd("exit\n", 1);
 			ft_putstr_fd("exit: ", 2);
 			ft_putstr_fd(str, 2);
-			ft_putstr_fd(": numeric argument required.\n", 2);
-			return (2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			return (255);
 		}
 	}
 	return (ft_atoi(str) % 256);
@@ -40,17 +41,19 @@ int	ft_exit(t_cmd *cmd)
 {
 	int	status;
 
-	if (cmd->argv[1] && cmd->argv[2])
-	{
-		ft_putstr_fd("exit: too many arguments.\n", 2);
-		return (1);
-	}
 	status = ft_strnum(cmd->argv[1]);
+	if (status != 255 && cmd->argv[1] && cmd->argv[2])   // all the subsecent arguments may leak ?
+	{
+		ft_putstr_fd("exit\n", 1);
+		ft_putstr_fd("exit: too many arguments\n", 2);
+		return(1);
+	}
 	ft_free_cmd(cmd);
 	free(g_vars->pids);
 	reset_terminal(g_vars);
 	ft_free_env(g_vars->env);
 	free(g_vars);
-	ft_putstr_fd("exit\n", 1);
+	if (status != 255)
+		ft_putstr_fd("exit\n", 1);
 	exit(status);
 }
