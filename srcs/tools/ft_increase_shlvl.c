af2f4a1e4144c55ead0141e9f53c0e09b445dd42
Ross <floriandevigne1@hotmail.fr>
2022-08-18 12:03:45 +0200

    fixing SHLVL

--- srcs/tools/ft_increase_shlvl.c
+++ srcs/tools/ft_increase_shlvl.c
@@ -6,7 +6,7 @@
 /*   By: fdevigne <fdevigne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 19:48:26 by fdevigne          #+#    #+#             */
-/*   Updated: 2022/08/17 18:20:18 by fdevigne         ###   ########.fr       */
+/*   Updated: 2022/08/18 11:59:19 by fdevigne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_increase_shlvl(t_env	*env)
 {
-	while (env && ft_strcmp2(env->key, "SHLVL"))
+	while (env->next && ft_strcmp2(env->key, "SHLVL"))
 		env = env->next;
 	if (!ft_strcmp2(env->key, "SHLVL"))
 		return (ft_atoi(env->value) + 1);
